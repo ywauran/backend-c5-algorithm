6c2e38ecdddbaabde3e0bb9b7b95699c248feb73
Yohanes Harke Wauran <harkewauran23@gmail.com>
2023-05-15 14:42:04 +0800

    done

--- app.py
+++ app.py
@@ -1,17 +1,27 @@
 from flask import Flask, request, jsonify
 from joblib import load
+from flask_cors import CORS # import CORS
 
 model = load('./models/model.joblib')
 
 app = Flask(__name__)
+CORS(app) # enable CORS for your app
 
 @app.route('/predict', methods=['POST'])
 def predict():
     data = request.json['data']
 
     prediction = model.predict(data)
+    feature_importances = model.feature_importances_.tolist()
+    node_impurities = model.tree_.impurity.tolist()
 
-    return jsonify({'prediction': prediction.tolist()})
+    response = {
+        'prediction': prediction.tolist(),
+        'feature_importances': feature_importances,
+        'node_impurities': node_impurities
+    }
+
+    return jsonify(response)
 
 @app.route('/')
 def index():
